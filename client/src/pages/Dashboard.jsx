a4791db67f9cd058ae7706b4380899bf565ac583
Brajendra Suman <s.brajendra@iitg.ac.in>
2023-08-20 01:06:15 +0530

    DashBoard modified

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,18 @@
+// redux
+import { useEffect, useState } from "react";
+import {TransactionContext} from "../context/TransactionContext"
+import React, { useContext } from 'react';
+
+
+
+
+const Dashboard = () => {
+
+  return (
+    <div>
+Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;

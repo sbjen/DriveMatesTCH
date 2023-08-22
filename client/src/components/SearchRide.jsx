a45c34e62ac7c528290e2762cd5903b2ed5682f9
Brajendra Suman <s.brajendra@iitg.ac.in>
2023-08-22 17:18:08 +0530

    Functionality improved

--- client/src/components/SearchRide.jsx
+++ client/src/components/SearchRide.jsx
@@ -3,41 +3,79 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 
-export default function SearchRide({ children }) {
+export default function SearchRide({
+  onSubmit,
+  city,
+  day,
+  month,
+  year,
+  handleInputChange,
+}) {
   return (
-    <Form className="search-ride-form ">
+    <Form className="search-ride-form " onSubmit={onSubmit}>
       <h4 style={{ marginBottom: "30px", marginTop: "30px" }}>
         {" "}
         Search Available Rides
       </h4>
 
-      <Form.Group className="mb-3" controlId="formGridAddress2">
+      <Form.Group className="mb-3" controlId="city">
         <Form.Label>City</Form.Label>
 
-        <Form.Select defaultValue="Choose...">
-          <option>Guwahati</option>
-          <option>Mumbai</option>
-          <option>Pune</option>
-          <option>Banglore</option>
-          <option>Delhi</option>
-          <option>Hyderabad</option>
+        <Form.Select
+          name="city"
+          value={city}
+          onChange={handleInputChange}
+          required
+          defaultValue="guwahati"
+        >
+          <option value="guwahati">Guwahati</option>
+          <option value="Mumbai">Mumbai</option>
+          <option value="Pune">Pune</option>
+          <option value="Banglore">Banglore</option>
+          <option value="Delhi">Delhi</option>
+          <option value="Hyderabad">Hyderabad</option>
         </Form.Select>
       </Form.Group>
 
       <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridCity">
+        <Form.Group as={Col} controlId="day">
           <Form.Label>Day</Form.Label>
-          <Form.Control placeholder="01" />
+          <Form.Control
+            placeholder="01"
+            type="number"
+            name="day"
+            value={day}
+            onChange={handleInputChange}
+            defaultValue="07"
+            required
+          />
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridState">
+        <Form.Group as={Col} controlId="month">
           <Form.Label>Month</Form.Label>
-          <Form.Control placeholder="01" />
+          <Form.Control
+            placeholder="01"
+            name="month"
+            type="number"
+            value={month}
+            defaultValue="07"
+            onChange={handleInputChange}
+            required
+          />
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridZip">
+        <Form.Group as={Col} controlId="year">
           <Form.Label>Year</Form.Label>
-          <Form.Control placeholder="2023" variant="dark" />
+          <Form.Control
+            placeholder="2023"
+            name="year"
+            type="number"
+            defaultValue="2002"
+            value={year}
+            onChange={handleInputChange}
+            required
+            variant="dark"
+          />
         </Form.Group>
       </Row>
 
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,25 +1,93 @@
+import React, { useState, useEffect } from "react";
+import SearchRide from "../components/SearchRide";
+import RideCard from "../components/RideCard";
+import { Col, Container, Row } from "react-bootstrap";
 import {
   MDBTabs,
   MDBTabsItem,
   MDBTabsLink,
   MDBTabsContent,
   MDBTabsPane,
-  MDBIcon,
 } from "mdb-react-ui-kit";
-import Profile from "../components/Profile";
-import RideCard from "../components/RideCard";
-import SearchRide from "../components/SearchRide";
+import Web3 from "web3";
+import { CONTRACT_ADDRESS, ABICODE } from "../assets/constants";
 
-import React, { useState, useContext, useEffect } from "react";
+// import Profile from "../components/Profile";
 
-import { TransactionContext } from "../context/TransactionContext";
-import { Col, Container, Row } from "react-bootstrap";
-
-let buttonStyle;
+// import { TransactionContext } from "../context/TransactionContext";
 
 export default function Dashboard() {
   const [tabCss, setTabCss] = useState([[], [], []]);
   const [basicActive, setBasicActive] = useState("tab1");
+  const [accounts, setAccounts] = useState([]);
+  const [contract, setContract] = useState(null);
+  const [web3, setWeb3] = useState(null);
+  const [userName, setUserName] = useState("");
+  const [userFound, setUserFound] = useState(false);
+  const [searchFormData, setSearchFormData] = useState({
+    city: "",
+    day: "",
+    month: "",
+    year: "",
+  });
+
+  // checking metamask metamask interface instance
+  useEffect(() => {
+    const initialize = async () => {
+      // Check if web3 is injected by the browser (Mist/MetaMask)
+      if (typeof window.ethereum !== "undefined") {
+        try {
+          await window.ethereum.request({ method: "eth_requestAccounts" });
+          const web3 = new Web3(window.ethereum);
+          setWeb3(web3);
+          const accounts = await web3.eth.getAccounts();
+          setAccounts(accounts);
+          const contract = new web3.eth.Contract(ABICODE, CONTRACT_ADDRESS);
+          setContract(contract);
+          console.log("------contracts--------");
+          console.log(contract);
+          const username = await contract.methods
+            .getUserName(accounts[0])
+            .call();
+          console.log(`userName deteccted on Network: ${username}`);
+          setUserName(username);
+          setUserFound(true);
+        } catch (error) {
+          console.error("Error initializing Web3:", error);
+          alert(
+            "either metamsk is not installed or you connected to different blockchain network."
+          );
+        }
+      } else {
+        console.log("Please install MetaMask!");
+      }
+    };
+    if (accounts.length === 0) {
+      initialize();
+    }
+  }, []);
+
+  // serach ride function
+  const searchRides = async (contract) => {
+    console.log(contract);
+    let CITY_DATE_CODE =
+      searchFormData.city.toLowerCase() +
+      searchFormData.day +
+      searchFormData.month +
+      searchFormData.year;
+    console.log(CITY_DATE_CODE);
+    const tx = await contract.methods
+      .searchRides("guwahati07072002")
+      .send({ from: accounts[0] });
+
+    // call transaction doesnt return reciept so cant get events from it
+
+    const events = tx;
+    const RIDES_IN_CITY = [];
+
+    console.log("-------------events--------------");
+    console.log(events);
+  };
 
   const handleBasicClick = (value) => {
     if (value === basicActive) {
@@ -35,7 +103,24 @@ export default function Dashboard() {
     setBasicActive(value);
   };
 
-  <i class="fas fa-car-side"></i>;
+  // searcha form handling
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setSearchFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form data submitted:", searchFormData);
+    searchRides(contract);
+    // loading all rides
+  };
+
+  // <i class="fas fa-car-side"></i>;
   return (
     <>
       <MDBTabs justify className="mb-6 ">
@@ -81,9 +166,14 @@ export default function Dashboard() {
                 md={4}
               >
                 <div>
-                 
-                  <SearchRide></SearchRide>
-
+                  <SearchRide
+                    onSubmit={handleSubmit}
+                    city={searchFormData.city}
+                    day={searchFormData.day}
+                    month={searchFormData.month}
+                    year={searchFormData.year}
+                    handleInputChange={handleInputChange}
+                  ></SearchRide>
                 </div>
                 <br />
                 <br />
@@ -93,9 +183,7 @@ export default function Dashboard() {
                 <div style={{ height: "90vh", overflowY: "auto" }}>
                   <RideCard></RideCard>
                   <RideCard></RideCard>
-
                   <RideCard></RideCard>
-
                   <RideCard></RideCard>
                   <RideCard></RideCard>
                   <RideCard></RideCard>
@@ -106,11 +194,9 @@ export default function Dashboard() {
                 className="take-min-height available-rides-deatails-col"
                 md={8}
               >
-                <Container fluid >
+                <Container fluid>
                   <Row>
-                    <Col flex>
-fdsf
-                    </Col>
+                    <Col flex>fdsf</Col>
                   </Row>
                 </Container>{" "}
               </Col>
